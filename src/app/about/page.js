be16971328900152e60221b5138fe4f1e81ba3aa
Jani B <aijala.j@gmail.com>
2024-07-30 09:58:59 +0300

    Adding more text to about me area. removing and adding images

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -16,13 +16,34 @@ export default function Projects() {
 
           <div className={styles.aboutMe}>
             <p>
-              Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              I am self taught front end engineer and I love to code on my freetime. I enjoy new challenges and love to find different approaches to problems.
+              Coding has been my hoppy for some time and decided to take it to the next level by taking multible courses on CSS, Javascript and React.
+
+              I do like to find new small challenges to keep my knowledge updated. One of the best places I like to challenge my skills is Frontend mentor site, where there is good projects to challenge the skills I have.
+              
                 
             </p>
+            
+          </div>
+          <div className={styles.skills}>
+            <h2>Skills</h2>
+            
+            <ul className={styles.skillsList}>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Python</li>
+              <li>Node js</li>
+            </ul>
+          
+
+            <h2>Courses</h2>
+              <p> 
+                Front End - Engineer  : CodeCademy <br /><br />
+                <Image className={styles.image} src="/images/front-end-sertificate.jpg" width="1000" height="1000" alt="code image"/>
+                <br /><br />
+              </p>
 
           </div>
         </div>
