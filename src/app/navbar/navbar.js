c27384207cb86961d801e2fb274180e6ad77fa95
Jani B <aijala.j@gmail.com>
2024-08-02 19:45:38 +0300

    responsive navbar with hamburger menu

--- src/app/navbar/navbar.js
+++ src/app/navbar/navbar.js
@@ -1,22 +1,45 @@
-import styles from "./navbar.module.css";
-import Link from 'next/link';
+"use client";
 
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
-    return (
-      <>
-        <div className={styles.main}>
+  const [menu, setMenu] = useState(styles.nav);
+
+  const menuChange = () => {
+    if (menu === styles.nav) {
+      setMenu(styles.navResponsive);
+    } else {
+      setMenu(styles.nav);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.main}>
         <div className={styles.logo}>
-            <p>Bergholm Portfolio</p>
+          <p>Bergholm Portfolio</p>
         </div>
-        <div className={styles.nav}>
+        <div className={menu} id={styles.myLinks}>
           <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/projects">Projects</Link></li>
-            <li><Link href="/about">About</Link></li>
+            <li className={styles.normal}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.normal}>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li className={styles.normal}>
+              <Link href="/about">About</Link>
+            </li>
+            <li className={styles.icon}>
+              <Link href="javascript:void(0);" onClick={() => menuChange()}>
+                ☰
+              </Link>
+            </li>
           </ul>
         </div>
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
