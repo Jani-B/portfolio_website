569900db01e9a028b10eef14c624ec79baf47ae9
Jani B <aijala.j@gmail.com>
2024-08-17 10:33:20 +0300

    added new links to about page and logos for the links

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -14,14 +14,14 @@ export default function About() {
 
         <div className={styles.aboutMe}>
           <p>
-            I am a self taught front end engineer and I love to code in my
-            freetime. I enjoy new challenges and love to find different
-            approaches to problems. Coding has been my hobby for some time and
-            decided to take it to the next level by taking multiple courses on
-            CSS, Javascript and React. I do like to find new small challenges to
-            keep my knowledge updated. One of the best places I like to
-            challenge my skills is the Frontend mentor site, where there are
-            good projects to challenge the skills I have.
+            I am a self taught front end engineer and I love to code in my free
+            time. I enjoy new challenges and love to find different approaches
+            to problems. Coding has been my hobby for some time and I decided to
+            take it to the next level by taking multiple courses on CSS,
+            Javascript and React. I do like to find new small challenges to keep
+            my knowledge updated. One of the best places I like to challenge my
+            skills is the Frontend mentor site, where there are good projects to
+            challenge the skills I have.
           </p>
         </div>
         <div className={styles.skills}>
@@ -35,9 +35,39 @@ export default function About() {
             <li>Python</li>
             <li>Node js</li>
           </ul>
+          <div className={styles.links}>
+            <h2>Links</h2>
+            <ul className={styles.skillsList}>
+              <li>
+                <a href="https://github.com/Jani-B" target="_blank">
+                  <Image
+                    className={styles.linkPictures}
+                    src="/images/GitHub_Logo.png"
+                    width="300"
+                    height="300"
+                    alt="github"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.frontendmentor.io/profile/Jani-B"
+                  target="_blank"
+                >
+                  <Image
+                    className={styles.linkPicturesMentor}
+                    src="/images/Frontend_mentor_logo.png"
+                    width="300"
+                    height="300"
+                    alt="github"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
 
           <h2>Courses</h2>
-          <p>Front End - Engineer : CodeCademy</p>
+          <p>Front - End Engineer : CodeCademy</p>
 
           <Image
             className={styles.image}
